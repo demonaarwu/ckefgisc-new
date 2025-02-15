b09f9c2e81c54027027cf2968800be2354928a70
Aaron Wu <demonaarwu@tuta.io>
2025-02-15 15:46:38 +0800

    Museum

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,5 @@
 import "./Home.css"
+import { Link } from "react-router-dom"
 import ckefgisc from "./img/ckefgisc.png"
 import Navbar from "./Navbar.tsx"
 import Footer from "./Footer.tsx"
@@ -27,20 +28,25 @@ function Questions() {
 					<p>
 						有幹部加入建北電資前完全沒碰過程式，在學長姐的教學下程式能力突飛猛進，甚至能自行開發大型專案或教其他人。
 					</p>
+					<p>
+						學長姐都很有耐心，願意聽你們提出的每個問題，所以不懂的地方都可以問學長姐喔！
+					</p>
 				</Question>
 				<Question title={"有學長學弟制/學姐學妹制嗎？"}>
 					<p>
 						<strong>完全沒有！</strong>和某些特定社團比起來，建北電資可說是完全沒有長姐制。社團內的每個人都是平等相處的，沒有誰高誰低。
+						也不會出現學長逼學弟留在學校補課、遇到學長姐一定要問好這些封建遺毒。
 					</p>
 					<p>
-						學長跟學姐也都是很友善、很講理的人，遇到不會的問題或想知道的事都可以請教我們喔！
+						學長跟學姐也都是很友善、講理的人，跟我們相處不用太拘束，就像一般朋友聊天就行！
 					</p>
 				</Question>
 				<Question title={"沒有電腦也可以嗎？"}>
 					<p>
 						<strong>可以！</strong>大社課跟放學小社課的上課地點都在電腦教室，可以使用建中或北一的電腦來上課。
 						<p>
-							不過，最好還是自己帶電腦來上課喔！有一些課程需要安裝軟體，而有些軟體無法在建中或北一的電腦上跑。
+							不過，最好還是自己帶電腦來上課喔！有一些課程需要安裝軟體，而這些軟體無法在建中或北一的電腦上跑。
+							帶自己的電腦來也能省下每次安裝程式的時間。
 						</p>
 					</p>
 				</Question>
@@ -74,17 +80,44 @@ function Intro() {
 					<img className="logo" src={ckefgisc} alt="" />
 				</div>
 				<h1>關於建北電資</h1>
-				<p>建北電資 = 建中電研 + 北一資研</p>
 				<p>
-					建北電資是一個由程式愛好者所組成的社團，由建中電研和北一資研兩社合併而來。
+					想<strong>提高程式能力</strong>、APCS 滿分嗎？想做出屬於自己的<strong>網站、遊戲</strong>嗎？
 				</p>
 				<p>
-					我們會在平日放學開設聯課，由兩社學術教授各種課程，內容包含 C++、Python、網頁設計和遊戲設計等。
+					想認識<strong>喜歡程式的朋友嗎</strong>？覺得男校/女校生活很無聊嗎？
 				</p>
+				<p>
+					那你要來<strong>加入建北電資</strong>！
+				</p>
+				<p>
+					<strong>建北電資</strong>是一個由程式愛好者所組成的社團，由建中電研和北一資研兩社合併而來，至今已是橫跨二十九年的大家庭。
+				</p>
+				<p>
+					建北電資有兩個主軸：<Link to="/courses">課程</Link>和<Link to="/events">活動</Link>。
+				</p>
+				<p>
+					我們會在平日放學開設聯課（<strong>小社課</strong>），由兩社學術教授各種課程，內容包含 C++、Python、網頁設計和遊戲設計等，橫跨網頁、遊戲、競程等主題。
+					下學期會在六月舉辦社展，展現一年多來的學習成果。
+				</p>
+				<p>
+					除了課程之外，我們還會在週末、寒暑假舉辦活動，以「<strong>四大獵祭</strong>」—春遊、暑訓、秋遊、寒訓為主，加上迎新、幹訓、遊等小型活動。
+					你能在活動認識各路電神、社牛，所以絕對不要錯過！
+				</p>
+				<CKEISCIntro/>
 				<Questions />
 			</Container>
 		</>
 	)
 }
+
+function CKEISCIntro() {
+	return (
+		<div className="ckeisc-intro">
+			<h2>關於建中電研</h2>
+		</div>
+	)
+}
+
+function 
 
 export default Home
--- src/museum/Museum.tsx
+++ src/museum/Museum.tsx
@@ -1,17 +1,66 @@
+import "./Museum.css"
 import Navbar from "../Navbar.tsx";
 import Container from "../Container.tsx";
 import Footer from "../Footer.tsx";
+import twenty_eight from "./img/28th.png";
+import twenty_seven from "./img/27th.png"
+import iscoj from "./img/iscoj.png"
+import ckeisc_history from "./img/ckeisc_history.png"
 
 function Museum() {
     document.title = "歷屆 | 建北電資 29th"
     return (
         <>
-            <Navbar/>
+            <Navbar />
             <Container>
-                <h1>Museum</h1>
+                <h1>歷屆博物館</h1>
+                <Website name="28th 社網" link="https://28th.ckefgisc.org" image={twenty_eight}>
+                    <p>
+                        鹽亞倫大電神架好了 27th 社網，好強。接著換 28th 上幹了，網管 晴 決定重頭寫自己的社網。
+                    </p>
+                    <p>
+                        雖然有學長姊的範本可以借鑑，不過因為他是笨，花了大半個暑假也沒有什麼進度。
+                        不過在某一天意外學會了 React 之後，事情變得不同了。
+                        有了進階網頁框架的強大功能，晴只需要把六哥寫好的前端網頁做成模板整理，接上 Router！於是 28th 社網就這麼誕生了……
+                    </p>
+                </Website>
+                <Website name="27th 社網" link="https://27th.ckefgisc.org" image={twenty_seven}>
+                    <p>
+                        建北電資以往皆有架設網站作為招生及宣傳用途。但是自從建電社辦的伺服器被學校沒收之後，一直以來都找不到一個良好的網站架設環境，也沒有一個地方讓學術們統一放置教材供學弟妹使用。
+                    </p>
+                    <p>
+                        因此，在一三接幹了之後，一二學術長檸檬便一直希望繼任的鹽亞倫可以將他們沒有做出的社網完成。 於是鹽亞倫便找了溫室菜以及北資學術長嗯嗯，嘗試從頭寫出一個網站，並且透過 Github Pages 進行架設。
+                    </p>
+                </Website>
+                <h1>其他網站</h1>
+                <Website name="ISCOJ" link="https://iscoj.ckefgisc.org" image={iscoj}>
+                    <p>
+                        建北電資的社員非常喜歡寫演算法題目，理所當然地我們有自己的線上評測系統，稱作 ISCOJ。不過 舊的 OJ 實在太難用了，於是前年（2023） AaW 和世宗架好了新的 OJ。
+                    </p>
+                </Website>
+                <Website name="建電社史追朔網" link="https://history.ckeisc.org" image={ckeisc_history}>
+                    <p>
+                        建國中學電子計算機研習社創社至今已過了四十餘年，今日的電研社在建中仍占有一席之地。然而，我們的社史已經在過去二十年間顛沛流離的社辦交換中遺失了，可憐的電研社至 2022 年前都沒有留下什麼整理過的社團歷史。
+                    </p>
+                    <p>
+                        直到 2023 年，終於在建電中出現了願意整理這些資料的一群人，開始挖掘往年的資料。透過學長姐的幫助，終於在雜亂的資料中整理出頭緒，建電的歷史正重新被寫下。
+                    </p>
+                </Website>
             </Container>
-            <Footer/>
+            <Footer />
         </>
+    )
+}
+
+function Website({ name, link, image, children }: { name: string, link: string, image: string, children: any }) {
+    return (
+        <div className="website">
+            <a href={link} target="_blank"><h2>{name}</h2></a>
+            <div className="website-image">
+                <img src={image} alt="" />
+            </div>
+            {children}
+        </div>
     )
 }
 
